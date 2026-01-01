24004508cada23ce759987c6d909505d969f6e98
Youssef Abdellaoui <kenicastrocantabria@gmail.com>
2026-01-01 13:33:38 +0100

    Initial Commit

---
+++ MomentumBar/MomentumBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumBarApp.swift
+//  MomentumBar
+//
+//  Created by Youssef Abdellaoui on 01.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MomentumBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MomentumBarTests/MomentumBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumBarTests.swift
+//  MomentumBarTests
+//
+//  Created by Youssef Abdellaoui on 01.01.26.
+//
+
+import Testing
+@testable import MomentumBar
+
+struct MomentumBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
